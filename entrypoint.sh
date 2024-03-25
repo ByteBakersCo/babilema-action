895dbc777f76c8059e0af89a7cef1b0f0bbfda5e
Arthur Wallendorff <howdy@wallenart.dev>
2024-03-25 17:36:50 +0100

    feat: implement most logic + rename entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,25 @@
 # Making sure that the script will exit if a command fails
 set -e
 
+git clone https://github.com/ByteBakersCo/babilema.git tmp
+
+cd tmp
+
+./build.sh
+
+mv babilema $GITHUB_WORKSPACE/
+
+cd $GITHUB_WORKSPACE
+
+rm -rf $GITHUB_WORKSPACE/tmp
+
+$GITHUB_WORKSPACE/babilema --config "$CONFIG"
+
 git config --global user.name "Babilema GH Action"
 git config --global user.email "action@github.com"
 
-./babilema --config "$INPUT_CONFIG"
-
 git add -A
-git commit -m "$INPUT_COMMIT_MESSAGE"
+git commit -m "$COMMIT_MESSAGE"
 git push https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 
 echo "Successfully pushed generated files to $GITHUB_REPOSITORY"
