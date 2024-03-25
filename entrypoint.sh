95e7b70467906944e74af87cd3076b014af9bde5
Arthur Wallendorff <howdy@wallenart.dev>
2024-03-25 22:22:01 +0100

    ci: temporary test (act has no context :( )

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,26 +3,35 @@
 # Making sure that the script will exit if a command fails
 set -e
 
-git clone https://github.com/ByteBakersCo/babilema.git tmp
-cd tmp
-./build.sh
+IS_PULL_REQUEST=$(echo "$GITHUB_CONTEXT" | jq -e '.event.pull_request' > /dev/null && echo "true" || echo "false")
 
-mv babilema "$GITHUB_WORKSPACE/"
-cd "$GITHUB_WORKSPACE"
-rm -rf "$GITHUB_WORKSPACE/tmp"
+if [  "$IS_PULL_REQUEST" = "true" ]; then
+    BRANCH=$(echo "$GITHUB_CONTEXT" | jq -r '.event.pull_request.head.ref')
+else
+    BRANCH=$(echo "$GITHUB_CONTEXT" | jq -r '.ref' | jq -R -r 'split("/") | last')
+fi
 
-$GITHUB_WORKSPACE/babilema --config "$CONFIG"
-rm "$GITHUB_WORKSPACE/babilema"
+echo $BRANCH
 
-git config --global user.name "Babilema GH Action"
-git config --global user.email "action@github.com"
-git config --global --add safe.directory "$GITHUB_WORKSPACE"
+# git clone https://github.com/ByteBakersCo/babilema.git tmp
+# cd tmp
+# ./build.sh
 
-echo "Checking out $GITHUB_REF_NAME"
-git checkout "$GITHUB_REF_NAME" "remote/$GITHUB_REF_NAME"
+# mv babilema "$GITHUB_WORKSPACE/"
+# cd "$GITHUB_WORKSPACE"
+# rm -rf "$GITHUB_WORKSPACE/tmp"
 
-git add -A
-git commit -m "$COMMIT_MESSAGE"
-git push https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+# $GITHUB_WORKSPACE/babilema --config "$CONFIG"
+# rm "$GITHUB_WORKSPACE/babilema"
+
+# git config --global user.name "Babilema GH Action"
+# git config --global user.email "action@github.com"
+# git config --global --add safe.directory "$GITHUB_WORKSPACE"
+
+# git checkout -b "$BRANCH"
+
+# git add -A
+# git commit -m "$COMMIT_MESSAGE"
+# git push https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 
 echo "Successfully pushed generated files to $GITHUB_REPOSITORY"
