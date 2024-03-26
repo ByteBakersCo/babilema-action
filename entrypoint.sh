963fe15259cd686d6ad5d7d9b6a69999bfc2b454
Arthur Wallendorff <howdy@wallenart.dev>
2024-03-26 17:48:26 +0100

    chore: remove more git related stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,8 @@
 # Making sure that the script will exit if a command fails
 set -e
 
-if [ -z "$GITHUB_CONTEXT" ]; then
-    echo "GITHUB_CONTEXT is not set."
-    exit 1
-fi
-
 if [ -z "$GITHUB_TOKEN" ]; then
     echo "GITHUB_TOKEN is not set."
-    exit 1
-fi
-
-BRANCH=$(echo "$GITHUB_CONTEXT" | jq -r '.ref' | jq -R -r 'split("/") | last')
-EVENT_NAME=$(echo "$GITHUB_CONTEXT" | jq -r '.event_name')
-
-if [ "$EVENT_NAME" = "pull_request" ]; then
-    BRANCH=$(echo "$GITHUB_CONTEXT" | jq -r '.head_ref')
-fi
-
-if [ -z "$BRANCH" ]; then
-    echo "Could not determine the branch."
     exit 1
 fi
 
